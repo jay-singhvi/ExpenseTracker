bdb9955376042b15f49f955e0df271f2347e30ce
Pratik Kaje <kajepratik@gmail.com>
2023-12-25 19:19:02 +0400

    INFRA: Initialize Unit Tests Project

---
+++ ExpenseTracker.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace ExpenseTracker.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(condition: true);
+    }
+}
