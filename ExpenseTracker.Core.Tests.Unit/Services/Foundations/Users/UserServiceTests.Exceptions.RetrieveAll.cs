1f1b886da5ad766dba1245b254342b2c81a8f68b
Pratik Kaje <kajepratik@gmail.com>
2024-02-15 20:57:11 +0400

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlExceptionOccursAndLogItAsync -> FAIL

---
+++ ExpenseTracker.Core.Tests.Unit/Services/Foundations/Users/UserServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,61 @@
+﻿using ExpenseTracker.Core.Models.Users;
+using ExpenseTracker.Core.Models.Users.Exceptions;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExpenseTracker.Core.Tests.Unit.Services.Foundations.Users
+{
+    public partial class UserServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlExceptionOccursAndLogItAsync()
+        {
+            // Given
+            User someUser = CreateRandomUser();
+            User storageUser = someUser;
+            User expectedUser = storageUser;
+
+            SqlException sqlException = GetSqlException();
+
+            var failedUserStorageException = 
+                new FailedUserStorageException(sqlException);
+
+            var expectedUserDependencyException = 
+                new UserDependencyException(failedUserStorageException);
+
+            this.userManagerBrokerMock.Setup(broker => 
+                broker.SelectAllUsers())
+                    .Throws(sqlException);
+
+            // Then
+            Action retrieveAllUsers = () => 
+                this.userService.RetrieveAllUsersAsync();
+
+            UserDependencyException actualUserDependencyException = 
+                Assert.Throws<UserDependencyException>(retrieveAllUsers);
+
+            actualUserDependencyException.Should()
+                .BeEquivalentTo(expectedUserDependencyException);
+
+            this.userManagerBrokerMock.Verify(broker =>
+                broker.SelectAllUsers(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedUserDependencyException))), 
+                        Times.Once);
+
+            this.userManagerBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
