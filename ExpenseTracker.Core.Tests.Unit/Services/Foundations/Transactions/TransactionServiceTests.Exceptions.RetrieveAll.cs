ada74adef138ca9b9927a26e407d4f4d83077a30
Pratik Kaje <kajepratik@gmail.com>
2024-02-25 22:34:19 +0400

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlExceptionErrorOccursAndLogItAsync -> FAIL

---
+++ ExpenseTracker.Core.Tests.Unit/Services/Foundations/Transactions/TransactionServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,56 @@
+﻿using ExpenseTracker.Core.Models.Transactions.Exceptions;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExpenseTracker.Core.Tests.Unit.Services.Foundations.Transactions
+{
+    public partial class TransactionServiceTests
+    {
+        [Fact]
+        public async void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlExceptionErrorOccursAndLogItAsync()
+        {
+            // Given
+            var sqlException = GetSqlException();
+
+            var failedTransactionStorageException = 
+                new FailedTransactionStorageException(sqlException);
+
+            var expectedTransactionDependencyException = 
+                new TransactionDependencyException(failedTransactionStorageException);
+
+            this.storageBrokerMock.Setup(broker => 
+                broker.SelectAllTransactions())
+                    .Throws(sqlException);
+
+            // When
+            Action retrieveAllTransactions = () => 
+                this.transactionService.RetrieveAllTransactions();
+
+            var actualTransactionDependencyException = 
+                Assert.Throws<TransactionDependencyException>(retrieveAllTransactions);
+
+            // Then
+            actualTransactionDependencyException.Should()
+                .BeEquivalentTo(expectedTransactionDependencyException);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.SelectAllTransactions(), 
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedTransactionDependencyException))), 
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
