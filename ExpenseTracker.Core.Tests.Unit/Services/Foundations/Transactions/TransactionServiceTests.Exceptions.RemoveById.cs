ebc684ea2053499db6e4aaf1e40c48ca74813a17
Pratik Kaje <kajepratik@gmail.com>
2024-03-02 22:52:56 +0400

    CODE RUB: Dependency upgrade

---
+++ ExpenseTracker.Core.Tests.Unit/Services/Foundations/Transactions/TransactionServiceTests.Exceptions.RemoveById.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExpenseTracker.Core.Tests.Unit.Services.Foundations.Transactions
+{
+    public partial class TransactionServiceTests
+    {
+        //[Fact]
+        //public async void ShouldThrowCriticalDependencyExceptionOnRemoveByIdIfSqlErrorOccursAndLogItAsync()
+        //{
+        //    // Given
+        //    var sqlExcep
+
+        //    // When
+
+        //    // Then
+        //}
+    }
+}
