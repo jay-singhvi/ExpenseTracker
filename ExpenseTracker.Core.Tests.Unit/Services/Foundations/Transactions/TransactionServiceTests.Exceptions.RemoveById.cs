754c232e1983a9da54931ba859aca76eafa7ae7f
Pratik Kaje <kajepratik@gmail.com>
2024-03-02 23:55:11 +0400

    ShouldThrowCriticalDependencyExceptionOnRemoveByIdIfSqlErrorOccursAndLogItAsync -> PASS

--- ExpenseTracker.Core.Tests.Unit/Services/Foundations/Transactions/TransactionServiceTests.Exceptions.RemoveById.cs
+++ ExpenseTracker.Core.Tests.Unit/Services/Foundations/Transactions/TransactionServiceTests.Exceptions.RemoveById.cs
@@ -1,16 +1,57 @@
-﻿namespace ExpenseTracker.Core.Tests.Unit.Services.Foundations.Transactions
+﻿using ExpenseTracker.Core.Models.Transactions;
+using ExpenseTracker.Core.Models.Transactions.Exceptions;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExpenseTracker.Core.Tests.Unit.Services.Foundations.Transactions
 {
     public partial class TransactionServiceTests
     {
-        //[Fact]
-        //public async void ShouldThrowCriticalDependencyExceptionOnRemoveByIdIfSqlErrorOccursAndLogItAsync()
-        //{
-        //    // Given
-        //    var sqlExcep
+        [Fact]
+        public async void ShouldThrowCriticalDependencyExceptionOnRemoveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // Given
+            Guid randomTransactionId = Guid.NewGuid();
+            var sqlException = GetSqlException();
 
-        //    // When
+            var failTransactionStorageException = 
+                new FailedTransactionStorageException(sqlException);
 
-        //    // Then
-        //}
+            var expectedTransactionDependencyException = 
+                new TransactionDependencyException(failTransactionStorageException);
+
+            this.storageBrokerMock.Setup(broker => 
+                broker.SelectTransactionByIdAsync(randomTransactionId))
+                    .ThrowsAsync(sqlException);
+
+            // When
+            ValueTask<Transaction> removeTransactionByIdTask = 
+                this.transactionService.RemoveTransactionByIdAsync(randomTransactionId);
+
+            var actualTransactionDependencyException = 
+                await Assert.ThrowsAsync<TransactionDependencyException>(
+                    removeTransactionByIdTask.AsTask);
+
+            // Then
+            actualTransactionDependencyException.Should()
+                .BeEquivalentTo(expectedTransactionDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectTransactionByIdAsync(
+                    It.IsAny<Guid>()),
+                        Times.Once);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedTransactionDependencyException))), 
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
