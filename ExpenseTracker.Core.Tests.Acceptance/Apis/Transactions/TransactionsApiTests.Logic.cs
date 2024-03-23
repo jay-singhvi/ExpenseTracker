5b4d7b778b042213370c79350bc79f5af1ecbd68
Pratik Kaje <kajepratik@gmail.com>
2024-03-24 00:08:42 +0400

    WIP: Removed Acceptance Test.

--- ExpenseTracker.Core.Tests.Acceptance/Apis/Transactions/TransactionsApiTests.Logic.cs
+++ ExpenseTracker.Core.Tests.Acceptance/Apis/Transactions/TransactionsApiTests.Logic.cs
@@ -10,36 +10,36 @@ namespace ExpenseTracker.Core.Tests.Acceptance.Apis.Transactions
 {
     public partial class TransactionsApiTests
     {
-        [Fact]
-        public async void ShouldPostTransactionAsync()
-        {
-            // Given
-            User randomUser = CreateRandomUser();
-            Transaction randomTransaction = CreateRandomTransaction();
-            Transaction inputTransaction = randomTransaction;
-            Transaction expectedTransaction = inputTransaction;
+    //    [Fact]
+    //    public async void ShouldPostTransactionAsync()
+    //    {
+    //        // Given
+    //        User randomUser = CreateRandomUser();
+    //        Transaction randomTransaction = CreateRandomTransaction();
+    //        Transaction inputTransaction = randomTransaction;
+    //        Transaction expectedTransaction = inputTransaction;
 
-            // When
-            await this.apiBroker.PostUserAsync(randomUser);
+    //        // When
+    //        await this.apiBroker.PostUserAsync(randomUser);
 
-            await this.apiBroker.LoginUserAsync(randomUser);
+    //        await this.apiBroker.LoginUserAsync(randomUser);
 
-            //FindUser
-            Guid userId = this.apiBroker.
+    //        //FindUser
+    //        Guid userId = this.apiBroker.
 
-            //inputTransaction.UserId = storageUser.Id;
+    //        //inputTransaction.UserId = storageUser.Id;
 
-            //var json = JsonConvert.SerializeObject(inputTransaction);
+    //        //var json = JsonConvert.SerializeObject(inputTransaction);
 
-            await this.apiBroker.PostTransactionAsync(inputTransaction);
+    //        await this.apiBroker.PostTransactionAsync(inputTransaction);
 
-            // Then
+    //        // Then
 
-            var actualTransaction = await this.apiBroker.GetTransactionByIdAsync(inputTransaction.Id);
+    //        var actualTransaction = await this.apiBroker.GetTransactionByIdAsync(inputTransaction.Id);
 
-            actualTransaction.Should().BeEquivalentTo(expectedTransaction);
+    //        actualTransaction.Should().BeEquivalentTo(expectedTransaction);
 
-            await this.apiBroker.DeleteTransactionByIdAsync(inputTransaction.Id);
-        }
+    //        await this.apiBroker.DeleteTransactionByIdAsync(inputTransaction.Id);
+    //    }
     }
 }
