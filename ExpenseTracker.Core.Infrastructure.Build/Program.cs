8be972ced3518a266d0dcc588bd97f2cf5f34c20
Pratik Kaje <kajepratik@gmail.com>
2024-03-25 17:35:06 +0400

    INFRA: Initial Project Setup

---
+++ ExpenseTracker.Core.Infrastructure.Build/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpenseTracker.Core.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
